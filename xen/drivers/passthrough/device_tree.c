3975836e127bc9d9880ed48e5df4b3617804d757
Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
2020-12-18 15:43:20 +0200

    iommu/arm: Introduce iommu_add_dt_pci_sideband_ids API
    The main purpose of this patch is to add a way to register PCI device
    (which is behind the IOMMU) using the generic PCI-IOMMU DT bindings [1]
    before assigning that device to a domain.
    
    This behaves similarly to the existing iommu_add_dt_device API, except it
    handles PCI devices, and it is to be invoked from the add_device hook in the
    SMMU driver.
    
    The function of_map_id to translate an ID through a downstream mapping
    (which is also suitable for mapping Requester ID) was borrowed from Linux
    (v5.10-rc6) and updated according to the Xen code base.
    
    [1] https://www.kernel.org/doc/Documentation/devicetree/bindings/pci/pci-iommu.txt
    
    Signed-off-by: Oleksandr Tyshchenko <oleksandr_tyshchenko@epam.com>
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    v4->v5:
    * style: add newlines after variable declarations and before return in iommu.h
    * drop dev_iommu_fwspec_get() check in iommu_add_dt_pci_sideband_ids()
    * Actually, change the check from is_protected to fwspec. TODO: we may actually just want to remove the check.
    * rebase on top of "dynamic node programming using overlay dtbo" series
    * append U on 0xffffffff constant in dt_map_id()
    
    v3->v4:
    * wrap #include <asm/acpi.h> and if ( acpi_disabled ) in #ifdef CONFIG_ACPI
    * fix Michal's remarks about style, parenthesis, and print formats
    * remove !ops->dt_xlate check since it is already in iommu_dt_xlate helper
    * rename s/iommu_dt_pci_map_id/dt_map_id/ because it is generic, not specific
      to iommu
    * update commit description
    
    v2->v3:
    * new patch title (was: iommu/arm: Introduce iommu_add_dt_pci_device API)
    * renamed function
      from: iommu_add_dt_pci_device
      to: iommu_add_dt_pci_sideband_ids
    * removed stale ops->add_device check
    * iommu.h: add empty stub iommu_add_dt_pci_sideband_ids for !HAS_DEVICE_TREE
    * iommu.h: add iommu_add_pci_sideband_ids helper
    * iommu.h: don't wrap prototype in #ifdef CONFIG_HAS_PCI
    * s/iommu_fwspec_free(pci_to_dev(pdev))/iommu_fwspec_free(dev)/
    
    v1->v2:
    * remove extra devfn parameter since pdev fully describes the device
    * remove ops->add_device() call from iommu_add_dt_pci_device(). Instead, rely on
      the existing iommu call in iommu_add_device().
    * move the ops->add_device and ops->dt_xlate checks earlier
    
    downstream->v1:
    * rebase
    * add const qualifier to struct dt_device_node *np arg in dt_map_id()
    * add const qualifier to struct dt_device_node *np declaration in iommu_add_pci_device()
    * use stdint.h types instead of u8/u32/etc...
    * rename functions:
      s/dt_iommu_xlate/iommu_dt_xlate/
      s/dt_map_id/iommu_dt_pci_map_id/
      s/iommu_add_pci_device/iommu_add_dt_pci_device/
    * add device_is_protected check in iommu_add_dt_pci_device
    * wrap prototypes in CONFIG_HAS_PCI
    
    (cherry picked from commit 734e3bf6ee77e7947667ab8fa96c25b349c2e1da from
     the downstream branch poc/pci-passthrough from
     https://gitlab.com/xen-project/people/bmarquis/xen-arm-poc.git)

--- xen/drivers/passthrough/device_tree.c
+++ xen/drivers/passthrough/device_tree.c
@@ -164,6 +164,137 @@ static int iommu_dt_xlate(struct device *dev,
     return ops->dt_xlate(dev, iommu_spec);
 }
 
+#ifdef CONFIG_HAS_PCI
+int dt_map_id(const struct dt_device_node *np, uint32_t id,
+              const char *map_name, const char *map_mask_name,
+              struct dt_device_node **target, uint32_t *id_out)
+{
+    uint32_t map_mask, masked_id, map_len;
+    const __be32 *map = NULL;
+
+    if ( !np || !map_name || (!target && !id_out) )
+        return -EINVAL;
+
+    map = dt_get_property(np, map_name, &map_len);
+    if ( !map )
+    {
+        if ( target )
+            return -ENODEV;
+
+        /* Otherwise, no map implies no translation */
+        *id_out = id;
+        return 0;
+    }
+
+    if ( !map_len || (map_len % (4 * sizeof(*map))) )
+    {
+        printk(XENLOG_ERR "%s: Error: Bad %s length: %u\n", np->full_name,
+               map_name, map_len);
+        return -EINVAL;
+    }
+
+    /* The default is to select all bits. */
+    map_mask = 0xffffffffU;
+
+    /*
+     * Can be overridden by "{iommu,msi}-map-mask" property.
+     * If df_property_read_u32() fails, the default is used.
+     */
+    if ( map_mask_name )
+        dt_property_read_u32(np, map_mask_name, &map_mask);
+
+    masked_id = map_mask & id;
+    for ( ; (int)map_len > 0; map_len -= 4 * sizeof(*map), map += 4 )
+    {
+        struct dt_device_node *phandle_node;
+        uint32_t id_base = be32_to_cpup(map + 0);
+        uint32_t phandle = be32_to_cpup(map + 1);
+        uint32_t out_base = be32_to_cpup(map + 2);
+        uint32_t id_len = be32_to_cpup(map + 3);
+
+        if ( id_base & ~map_mask )
+        {
+            printk(XENLOG_ERR "%s: Invalid %s translation - %s-mask (0x%"PRIx32") ignores id-base (0x%"PRIx32")\n",
+                   np->full_name, map_name, map_name, map_mask, id_base);
+            return -EFAULT;
+        }
+
+        if ( (masked_id < id_base) || (masked_id >= (id_base + id_len)) )
+            continue;
+
+        phandle_node = dt_find_node_by_phandle(phandle);
+        if ( !phandle_node )
+            return -ENODEV;
+
+        if ( target )
+        {
+            if ( !*target )
+                *target = phandle_node;
+
+            if ( *target != phandle_node )
+                continue;
+        }
+
+        if ( id_out )
+            *id_out = masked_id - id_base + out_base;
+
+        printk(XENLOG_DEBUG "%s: %s, using mask %08"PRIx32", id-base: %08"PRIx32", out-base: %08"PRIx32", length: %08"PRIx32", id: %08"PRIx32" -> %08"PRIx32"\n",
+               np->full_name, map_name, map_mask, id_base, out_base, id_len, id,
+               masked_id - id_base + out_base);
+        return 0;
+    }
+
+    printk(XENLOG_ERR "%s: no %s translation for id 0x%"PRIx32" on %s\n",
+           np->full_name, map_name, id, (target && *target) ? (*target)->full_name : NULL);
+
+    /*
+     * NOTE: Linux bypasses translation without returning an error here,
+     * but should we behave in the same way on Xen? Restrict for now.
+     */
+    return -EFAULT;
+}
+
+int iommu_add_dt_pci_sideband_ids(struct pci_dev *pdev)
+{
+    const struct iommu_ops *ops = iommu_get_ops();
+    struct dt_phandle_args iommu_spec = { .args_count = 1 };
+    struct device *dev = pci_to_dev(pdev);
+    const struct dt_device_node *np;
+    int rc = NO_IOMMU;
+
+    if ( !iommu_enabled )
+        return NO_IOMMU;
+
+    if ( !ops )
+        return -EINVAL;
+
+    if ( dev_iommu_fwspec_get(dev) )
+        return 0;
+
+    np = pci_find_host_bridge_node(pdev);
+    if ( !np )
+        return -ENODEV;
+
+    /*
+     * According to the Documentation/devicetree/bindings/pci/pci-iommu.txt
+     * from Linux.
+     */
+    rc = dt_map_id(np, PCI_BDF(pdev->bus, pdev->devfn), "iommu-map",
+                   "iommu-map-mask", &iommu_spec.np, iommu_spec.args);
+    if ( rc )
+        return (rc == -ENODEV) ? NO_IOMMU : rc;
+
+    rc = iommu_dt_xlate(dev, &iommu_spec);
+    if ( rc < 0 )
+    {
+        iommu_fwspec_free(dev);
+        return -EINVAL;
+    }
+
+    return rc;
+}
+#endif /* CONFIG_HAS_PCI */
+
 int iommu_remove_dt_device(struct dt_device_node *np)
 {
     const struct iommu_ops *ops = iommu_get_ops();

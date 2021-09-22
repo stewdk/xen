01db28089a8e27bb1fda7566e45a0a3d9b7bf85a
Edward Pickup <Edward.Pickup@arm.com>
2021-09-22 14:44:44 +0100

    arm/pci: Add pci-scan boot argument
    This patch adds a Xen boot arguments that, if enabled, causes a call to
    existing code to scan pci devices enumerated by the firmware.
    
    This patch also makes an existing debug function viewable outside its
    translation unit, and uses this to dump the PCI devices found.
    The debug message is controlled by an additional configurable added to
    arch/arm/Kconfig.debug.
    
    Additionally, this patch modifies segment loading to ensure that PCI
    devices on other segments are properly found.
    
    This will be needed ahead of dom0less support for pci passthrough on
    arm.
    
    Issue-Id: SCM-1368
    Signed-off-by: Luca Fancellu <luca.fancellu@arm.com>
    Signed-off-by: Edward Pickup <Edward.Pickup@arm.com>
    Change-Id: I79eb1325019fb37fe0c7951070efe5745fed6905
    ---
    (cherry picked from commit bce463e1588a45e1bfdf59fc0d5f88b16604e439 from
     the downstream branch poc/pci-passthrough from
     https://gitlab.com/xen-project/people/bmarquis/xen-arm-poc.git)
    
    v3->v4 (xlnx):
    * s/always_inline/inline/
    
    v4->v5 (xlnx):
    * fixup debug Kconfig option s/CONFIG_//
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>

--- xen/include/xen/pci.h
+++ xen/include/xen/pci.h
@@ -163,6 +163,7 @@ bool_t __must_check pcidevs_locked(void);
 bool_t pci_known_segment(u16 seg);
 bool_t pci_device_detect(u16 seg, u8 bus, u8 dev, u8 func);
 int scan_pci_devices(void);
+void dump_pci_devices(unsigned char ch);
 enum pdev_type pdev_type(u16 seg, u8 bus, u8 devfn);
 int find_upstream_bridge(u16 seg, u8 *bus, u8 *devfn, u8 *secbus);
 

f6340a2237723f4161771c888de98b436d6acaad
Stewart Hildebrand <stewart.hildebrand@amd.com>
2022-11-10 10:19:00 -0500

    xen/vpci: header: filter PCI capabilities
    Currently, Xen vPCI only supports virtualizing the MSI and MSI-X capabilities.
    Hide all other PCI capabilities (including extended capabilities) from domUs for
    now, even though there may be certain devices/drivers that depend on being able
    to discover certain capabilities.
    
    We parse the physical PCI capabilities linked list and add vPCI register
    handlers for the next elements, inserting our own next value, thus presenting a
    modified linked list to the domU.
    
    Introduce helper functions vpci_hw_read8 and vpci_read_val. The vpci_read_val
    helper function returns a fixed value, which may be used for RAZ registers, or
    registers whose value doesn't change.
    
    Introduce pci_find_next_cap_ttl() helper while adapting the logic from
    pci_find_next_cap() to suit our needs, and implement the existing
    pci_find_next_cap() in terms of the new helper.
    
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    ---
    v6->v7:
    * no change
    
    v5->v6:
    * add register handlers before status register handler in init_bars()
    * s/header->mask_cap_list/mask_cap_list/
    
    v4->v5:
    * use more appropriate types, continued
    * get rid of unnecessary hook function
    * add Jan's R-b
    
    v3->v4:
    * move mask_cap_list setting to this patch
    * leave pci_find_next_cap signature alone
    * use more appropriate types
    
    v2->v3:
    * get rid of > 0 in loop condition
    * implement pci_find_next_cap in terms of new pci_find_next_cap_ttl function so
      that hypothetical future callers wouldn't be required to pass &ttl.
    * change NULL to (void *)0 for RAZ value passed to vpci_read_val
    * change type of ttl to unsigned int
    * remember to mask off the low 2 bits of next in the initial loop iteration
    * change return type of pci_find_next_cap and pci_find_next_cap_ttl
    * avoid wrapping the PCI_STATUS_CAP_LIST condition by using ! instead of == 0
    
    v1->v2:
    * change type of ttl to int
    * use switch statement instead of if/else
    * adapt existing pci_find_next_cap helper instead of rolling our own
    * pass ttl as in/out
    * "pass through" the lower 2 bits of the next pointer
    * squash helper functions into this patch to avoid transient dead code situation
    * extended capabilities RAZ/WI

--- xen/drivers/pci/pci.c
+++ xen/drivers/pci/pci.c
@@ -39,29 +39,37 @@ unsigned int pci_find_cap_offset(pci_sbdf_t sbdf, unsigned int cap)
     return 0;
 }
 
-unsigned int pci_find_next_cap(pci_sbdf_t sbdf, unsigned int pos,
-                               unsigned int cap)
+unsigned int pci_find_next_cap_ttl(pci_sbdf_t sbdf, unsigned int pos,
+                                   bool (*is_match)(unsigned int),
+                                   unsigned int cap, unsigned int *ttl)
 {
-    u8 id;
-    int ttl = 48;
+    unsigned int id;
 
-    while ( ttl-- )
+    while ( (*ttl)-- )
     {
         pos = pci_conf_read8(sbdf, pos);
         if ( pos < 0x40 )
             break;
 
-        pos &= ~3;
-        id = pci_conf_read8(sbdf, pos + PCI_CAP_LIST_ID);
+        id = pci_conf_read8(sbdf, (pos & ~3) + PCI_CAP_LIST_ID);
 
         if ( id == 0xff )
             break;
-        if ( id == cap )
+        if ( (is_match && is_match(id)) || (!is_match && id == cap) )
             return pos;
 
-        pos += PCI_CAP_LIST_NEXT;
+        pos = (pos & ~3) + PCI_CAP_LIST_NEXT;
     }
+
     return 0;
+}
+
+unsigned int pci_find_next_cap(pci_sbdf_t sbdf, unsigned int pos,
+                               unsigned int cap)
+{
+    unsigned int ttl = 48;
+
+    return pci_find_next_cap_ttl(sbdf, pos, NULL, cap, &ttl) & ~3;
 }
 
 /**

f997f39610738fc363224de35a6d3885312d3370
Jason Andryuk <jason.andryuk@amd.com>
2024-10-22 15:57:59 -0400

    vpci/msix: Expose complete MSI-X config space to domUs
    The vpci MSI-X handler was only exposing the first 4 of 12 bytes of the
    MSI-X config space.  Dom0 must fall back to default HW reading.  For a
    domU though this meant the PBA and MSI-X tables were returning 0xff.
    Since these values are invalid, the domU wouldn't try to use MSI-X.
    
    Latch the values and return them for a domU.  The fields are read-only
    and static, so they can be returned without consulting hardware.
    
    Signed-off-by: Jason Andryuk <jason.andryuk@amd.com>
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    This patch was borrowed from VAS and introduced in xlnx_rebase_4.20.
    Link to gerrit reviews:
    https://gerrit-git.amd.com/c/brahma/ec/xen/+/1146703
    https://gerrit-git.amd.com/c/brahma/ec/xen/+/1238864
    
    Since then, I've reworked the cleanup path slightly.

--- xen/drivers/vpci/msix.c
+++ xen/drivers/vpci/msix.c
@@ -610,9 +610,33 @@ static int cf_check init_msix(struct pci_dev *pdev)
     rc = vpci_add_register(pdev->vpci, control_read, control_write,
                            msix_control_reg(msix_offset), 2, msix);
     if ( rc )
+        goto out;
+
+    if ( !is_hardware_domain(d) )
     {
-        xfree(msix);
-        return rc;
+        unsigned long val;
+
+        val = pci_conf_read32(pdev->sbdf, msix_table_offset_reg(msix_offset));
+        rc = vpci_add_register(pdev->vpci, vpci_read_val, NULL,
+                               msix_table_offset_reg(msix_offset), 4,
+                               (void *)(uintptr_t)val);
+        if ( rc )
+        {
+            printk("%pd: %pp register msix_table_offset_reg failed\n",
+                   d, &pdev->sbdf);
+            goto out_control;
+        }
+
+        val = pci_conf_read32(pdev->sbdf, msix_pba_offset_reg(msix_offset));
+        rc = vpci_add_register(pdev->vpci, vpci_read_val, NULL,
+                               msix_pba_offset_reg(msix_offset), 4,
+                               (void *)(uintptr_t)val);
+        if ( rc )
+        {
+            printk("%pd: %pp register msix_pba_offset failed\n",
+                   d, &pdev->sbdf);
+            goto out_table;
+        }
     }
 
     msix->max_entries = max_entries;
@@ -633,7 +657,30 @@ static int cf_check init_msix(struct pci_dev *pdev)
      * p2m, as MSI-X capability was not yet initialized.  Crave a hole for
      * the MSI-X table here, so that Xen can trap accesses.
      */
-    return vpci_make_msix_hole(pdev);
+    rc = vpci_make_msix_hole(pdev);
+    if ( rc )
+        goto out_pba;
+
+    return 0;
+
+ out_pba:
+    if ( !vpci_remove_registers(pdev->vpci,
+                                msix_pba_offset_reg(msix_offset), 4) )
+        printk("%pd: %pp remove msix_pba_offset failed\n", d, &pdev->sbdf);
+
+ out_table:
+    if ( !vpci_remove_registers(pdev->vpci,
+                                msix_table_offset_reg(msix_offset), 4) )
+        printk("%pd: %pp remove msix_table_offset failed\n", d, &pdev->sbdf);
+
+ out_control:
+    if ( !vpci_remove_registers(pdev->vpci,
+                                msix_control_reg(msix_offset), 2) )
+        printk("%pd: %pp remove msix_control failed\n", d, &pdev->sbdf);
+
+ out:
+    xfree(msix);
+    return rc;
 }
 REGISTER_VPCI_CAP(MSIX, init_msix, NULL);
 
